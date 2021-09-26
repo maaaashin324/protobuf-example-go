0afebc07945772dafbdb32dbe3e74ffe8a8f5c16
maaaashin324 <masataka.shintoku@gmail.com>
2021-09-26 12:12:43 +0900

    Chore: Added enum_exmple.proto and converted that into Go code
    - Added enum_exmaple.proto
    - Converted that into Go code
    - Fixed how to use protoc with proper instructions, e.g. paths=source_relative

--- generate.sh
+++ generate.sh
@@ -1,1 +1,2 @@
-protoc -I src/ --go_out=src/ src/simple/simple.proto
+protoc -I src/ --go_out=src/ --go_opt=paths=source_relative src/simple/simple.proto
+protoc -I src/ --go_out=src/ --go_opt=paths=source_relative src/enum_example/enum_example.proto
