2b0b22204811d3c0527e00ae7cd261852952e51c
maaaashin324 <masataka.shintoku@gmail.com>
2021-09-26 18:32:56 +0900

    Chore: Added addressbook.proto and converted that into go code

--- generate.sh
+++ generate.sh
@@ -1,3 +1,4 @@
 protoc -I src/ --go_out=src/ --go_opt=paths=source_relative src/simple/simple.proto
 protoc -I src/ --go_out=src/ --go_opt=paths=source_relative src/enum_example/enum_example.proto
 protoc -I src/ --go_out=src/ --go_opt=paths=source_relative src/complex/complex.proto
+protoc -I src/ --go_out=src/ --go_opt=paths=source_relative src/addressbook/addressbook.proto
